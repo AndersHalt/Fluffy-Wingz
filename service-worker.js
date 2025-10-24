f6e7a86ef1449d8e6affb2f65f8c6671c5e2462e
AndersHalt <andershalt203@icloud.com>
2025-10-25 01:04:55 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='fluffy-angel-v1';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo.png','./bird_sprite.png'])))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&k.startsWith('fluffy-')?caches.delete(k):null))))});
+const CACHE='fluffy-wingz-v10';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo.png','./bird_sprite.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&k.startsWith('fluffy-wingz-')?caches.delete(k):null))))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
