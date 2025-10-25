ca7239ffbfeebb6a7cc948e7e1676ecfbd1e4271
AndersHalt <andershalt203@icloud.com>
2025-10-25 03:19:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE='fluffy-wingz-v10';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo.png','./bird_sprite.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&k.startsWith('fluffy-wingz-')?caches.delete(k):null))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE='fluffy-wingz-v12';const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo.png','./bird_sprite.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))) });self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&k.startsWith('fluffy-wingz-')?caches.delete(k):null))))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
